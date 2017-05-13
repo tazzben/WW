757539cabc5956f1aef59fa6e9cfceaa654a5fd2
Ben Smith <ben@wbpsystems.com>
2017-05-12 19:34:55 -0500

    Update to 1.9

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_desc = """
 
 
 setup(name="ww_out",
-      version=1.7,
+      version=1.9,
       description="A command line tool to disaggregate pre and post test responses into Walstad and Wagner learning types",
       author="Ben Smith",
       author_email="bosmith@unomaha.edu",
